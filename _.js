6bf3400b62c694abb75b9fb11e8c44af7b81adb9
mariangz <marianosgimenez@gmail.com>
2021-10-06 10:40:03 +0200

    add clamp

--- _.js
+++ _.js
@@ -1,2 +1,18 @@
+const _ = {
+  clamp(number, lowerBound, upperBound) {
+    // if (number < lowerBound) {
+    //   return lowerBound;
+    // } else if (number > upperBound) {
+    //   return upperBound;
+    // } else {
+    //   return number;
+    // }
+
+    const lowerClampedValue = Math.max(number, lowerBound);
+    const clampedValue = Math.min(lowerClampedValue, upperBound);
+    return clampedValue;
+  },
+};
+
 // Do not write or modify code below this line.
 module.exports = _;
